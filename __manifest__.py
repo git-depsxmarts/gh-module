a361a9f39d0d77af3c3f890e27a57099b2e244f3
git-depsxmarts <145722749+git-depsxmarts@users.noreply.github.com>
2024-11-07 20:47:30 -0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'gh-module',
+    'summary': 'Gh Module',
+    'version': '17.0',
+    'category': 'Sales Management',
+    'author': 'xmarts',
+    'website': '',
+    'license': '',
+    'depends': [],
+    'data': [],
+    'maintainers': [
+        'kuro088',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+
